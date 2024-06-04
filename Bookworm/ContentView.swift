939c3441d9f1974385666b59366e03ed10590295
Berserk-21 <cryptogekkofr@protonmail.com>
2024-06-04 10:08:03 +0200

    Uses @Binding to binds the state of two properties accross views

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOn = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        PushButton(title: isOn ? "Turn off" : "Turn on", isOn: $isOn)
+        .padding()
+    }
+}
+
+struct PushButton: View {
+    
+    let title: String
+    // Use Binding to binds this property to the one from the contentView. They now share the same state.
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
